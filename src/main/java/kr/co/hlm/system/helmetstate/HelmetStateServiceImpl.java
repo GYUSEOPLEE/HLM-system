cb5cf1a038e51a34150409b7414d8178f7e866ea
Lee Gyu Seop <gseoplee@gmail.com>
2022-05-18 10:38:05 +0900

    feat : Add HelmetStateMapping and others
    Add specific method getHelmetStates, getHelmetState
    Edit HelmetState in HelmetStateContloller
    Add code in HelmetStateMapper.xml

--- src/main/java/kr/co/hlm/system/helmetstate/HelmetStateServiceImpl.java
+++ src/main/java/kr/co/hlm/system/helmetstate/HelmetStateServiceImpl.java
@@ -1,21 +1,45 @@
 package kr.co.hlm.system.helmetstate;
 
-import java.util.List;
+import kr.co.hlm.system.helmet.Helmet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class HelmetStateServiceImpl implements HelmetStateService{
+    private final HelmetStateMapper helmetStateMapper;
+    public static Map<String, Character> helmetWear;
+
+    static {
+        helmetWear = new HashMap<String, Character>();
+    }
+
     @Override
     public void createHelmetState(HelmetState helmetState) {
-
+        helmetStateMapper.insert(helmetState);
     }
 
     @Override
     public List<HelmetState> getHelmetStates(HelmetState helmetState) {
-        return null;
+       List<HelmetState>  helmetStates = helmetStateMapper.selectAll(helmetState);
+
+       return helmetStates != null
+               ? helmetStates
+               : new ArrayList<HelmetState>();
     }
 
     @Override
     public HelmetState getHelmetState(HelmetState helmetState) {
-        return null;
+        HelmetState viewHelmetState = helmetStateMapper.select(helmetState);
+
+        return viewHelmetState != null
+                ? viewHelmetState
+                : new HelmetState();
     }
 
     @Override
