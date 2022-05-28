9805c335cff83cba5cae320a346be985c2e65431
Lee Gyu Seop <gseoplee@gmail.com>
2022-05-28 18:19:00 +0900

    feat : Setting method in HelmetLostUtil and ManagemnetService

--- src/main/java/kr/co/hlm/system/helmetstate/HelmetStateServiceImpl.java
+++ src/main/java/kr/co/hlm/system/helmetstate/HelmetStateServiceImpl.java
@@ -1,17 +1,19 @@
 package kr.co.hlm.system.helmetstate;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import kr.co.hlm.system.helmet.Helmet;
 import kr.co.hlm.system.helmet.HelmetMapper;
 import kr.co.hlm.system.helmet.HelmetServiceImpl;
-import kr.co.hlm.system.kickboard.Kickboard;
-import kr.co.hlm.system.kickboard.KickboardMapper;
 import kr.co.hlm.system.kickboardlocation.KickboardLocation;
 import kr.co.hlm.system.kickboardlocation.KickboardLocationMapper;
+import kr.co.hlm.system.management.HelmetLostUtil;
+import kr.co.hlm.system.management.ManagementService;
 import lombok.RequiredArgsConstructor;
+import okhttp3.*;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,29 +23,28 @@ import java.util.Map;
 public class HelmetStateServiceImpl implements HelmetStateService{
     private final HelmetMapper helmetMapper;
     private final HelmetStateMapper helmetStateMapper;
-    private final KickboardMapper kickboardMapper;
     private final KickboardLocationMapper kickboardLocationMapper;
     private final ApplicationEventPublisher applicationEventPublisher;
+    private final HelmetLostUtil helmetLostUtil;
+    private final ManagementService managementService;
     public static Map<String, Character> helmetWear = new HashMap<>();
 
     @Override
     public void createHelmetState(HelmetState helmetState) {
         Helmet helmet = new Helmet();
         helmet.setNo(helmetState.getHelmetNo());
-//        Helmet helmet = Helmet.builder()
-//                .no(helmetState.getHelmetNo()).build();
+        helmet = helmetMapper.select(helmet);
 
         KickboardLocation kickboardLocation = new KickboardLocation();
-        kickboardLocation.setKickboardNo(HelmetServiceImpl.helmetPair.get(helmetMapper.select(helmet).getNo()));
-//        KickboardLocation kickboardLocation = KickboardLocation.builder()
-//                .kickboardNo(HelmetServiceImpl.helmetPair.get(helmetMapper.select(helmet).getNo()))
-//                .build();
+        kickboardLocation.setKickboardNo(HelmetServiceImpl.helmetPair.get(helmet.getNo()));
 
         //T면 분실X F면 분실
-        if (helmetLostCalculation(helmetState, kickboardLocationMapper.select(kickboardLocation))) {
+        if (helmetLostUtil.helmetLostCalculation(helmetState, kickboardLocationMapper.select(kickboardLocation))) {
             helmetState.setLoss('N');
         } else {
             helmetState.setLoss('Y');
+
+            managementService.sendHelmetLoss(helmetMapper.select(helmet));
         }
 
         helmetState.setWear(helmetWear.get(helmetState.getHelmetNo()));
@@ -75,34 +76,5 @@ public class HelmetStateServiceImpl implements HelmetStateService{
     @Override
     public void editHelmetState(HelmetState helmetState) {
         helmetWear.put(helmetState.getHelmetNo(), helmetState.getWear());
-    }
-
-    private boolean helmetLostCalculation(HelmetState helmetState, KickboardLocation kickboardLocation){
-        boolean result = true;
-        double theta = helmetState.getLongitude() - kickboardLocation.getLongitude();
-        double dist = Math.sin(deg2rad(helmetState.getLatitude())) * Math.sin(deg2rad(kickboardLocation.getLatitude()))
-                + Math.cos(deg2rad(helmetState.getLatitude())) * Math.cos(deg2rad(kickboardLocation.getLatitude()))
-                * Math.cos(deg2rad(theta));
-
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist *= 60 * 1.1515;
-        dist *= 1609.344;
-
-        if(dist > 30){
-            result = false;
-        }
-
-        System.out.println(dist);
-
-        return result;
-    }
-
-    private double deg2rad(double deg) {
-        return (deg * Math.PI / 180.0);
-    }
-
-    private double rad2deg(double rad) {
-        return (rad * 180 / Math.PI);
     }
 }
